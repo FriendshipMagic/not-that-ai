8a69ec88c88acb0cb63cdaccfff197add8912c2e
bloodymoon <1270046784@qq.com>
2024-07-21 16:32:35 +0800

    chore: 文本修改

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     return (
         <html suppressHydrationWarning lang="en">
             <head>
-                <title>VirgilAI: Powerful AI</title>
+                <title>{siteConfig.name}</title>
             </head>
             <body className={clsx('min-h-screen bg-background font-sans antialiased', fontSans.variable)}>
                 <Providers themeProps={{ attribute: 'class', defaultTheme: 'dark' }}>
