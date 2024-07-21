5091f77dfb92a1cf1a81ffc67f5918e494b9b869
bloodymoon <1270046784@qq.com>
2024-07-21 20:53:16 +0800

    chore: 删除无用目录

--- app/feedback/page.tsx
+++
@@ -1,9 +0,0 @@
-import { title } from '@/components/primitives'
-
-export default function FeedbackPage() {
-    return (
-        <div>
-            <h1 className={title()}>Feedback</h1>
-        </div>
-    )
-}
