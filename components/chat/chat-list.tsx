143b0b9c0bf2ef5e96b498736ebe301a42cdf51a
bloodymoon <1270046784@qq.com>
2024-07-23 00:46:22 +0800

    feat: 初始创建历史对话列表

--- components/chat/chat-list.tsx
+++ components/chat/chat-list.tsx
@@ -1,11 +1,12 @@
 'use client'
 
 import { Listbox, ListboxItem } from '@nextui-org/listbox'
-import React from 'react'
+import { useState } from 'react'
 import { Link } from '@nextui-org/link'
 import { ScrollShadow } from '@nextui-org/scroll-shadow'
 import { Input } from '@nextui-org/input'
 import { PlusFilledIcon, SearchIcon } from '@nextui-org/shared-icons'
+import { HeartFilled, HeartOutlined } from '@ant-design/icons'
 
 function TopBar() {
     return (
@@ -25,6 +26,8 @@ function TopBar() {
 }
 
 export default function ChatList() {
+    const [love, setLove] = useState(false)
+
     return (
         <div className="flex flex-col">
             <TopBar />
@@ -35,11 +38,18 @@ export default function ChatList() {
                     defaultSelectedKeys="0"
                     onAction={(key) => console.log(key)}
                 >
-                    {new Array(35).fill(0).map((_, i) => (
-                        <ListboxItem key={i} textValue={i.toString()}>
-                            <Link color="foreground" href={'/chat'} size="lg" onClick={() => console.log(i)}>
-                                {i}
-                            </Link>
+                    {new Array(20).fill(0).map((_, i) => (
+                        <ListboxItem key={i} className="py-3" color="warning" textValue={i.toString()}>
+                            <div className="flex flex-row items-center space-x-3">
+                                {love ? (
+                                    <HeartFilled className="text-lg dark:text-white" onClick={() => setLove(!love)} />
+                                ) : (
+                                    <HeartOutlined className="text-lg dark:text-white" onClick={() => setLove(!love)} />
+                                )}
+                                <Link color="foreground" href={'/chat'} size="lg" onClick={() => console.log(i)}>
+                                    {i}
+                                </Link>
+                            </div>
                         </ListboxItem>
                     ))}
                 </Listbox>
